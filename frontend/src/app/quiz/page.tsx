9f8dc8f0887b16b7d4a5355de51225133f601182
Neil <ncagabriel02@gmail.com>
2025-12-11 19:41:07 +0800

    trial

--- frontend/src/app/quiz/page.tsx
+++ frontend/src/app/quiz/page.tsx
@@ -1,17 +1,17 @@
-import QuizForm from "../QuizForm";
-import { Questions } from "@/utils/types";
-import axios from "@/utils/axios.client";
+// import QuizForm from "../QuizForm";
+// import { Questions } from "@/utils/types";
+// import axios from "@/utils/axios.client";
 
 // SSR
-export default async function QuizPage() {
-  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL; // full URL to Worker
-  const res = await axios.get<Questions[]>(`${backendUrl}/quiz`);
-  const questions: Questions[] = res.data;
+export default async function page() {
+  // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL; // full URL to Worker
+  // const res = await axios.get<Questions[]>(`${backendUrl}/quiz`);
+  // const questions: Questions[] = res.data;
 
   return (
     <div className="p-4 max-w-xl mx-auto ">
       <h1 className="text-center text-2xl font-bold mb-4">Qing Quiz</h1>
-      <QuizForm questions={questions} />
+      {/* <QuizForm questions={questions} /> */}
     </div>
   );
 }
