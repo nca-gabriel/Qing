5ab51661719ed528bdbcb1271729fe13918bb797
Neil <ncagabriel02@gmail.com>
2025-12-11 19:29:04 +0800

    hotfix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  appDir: "src",
+} as unknown;
 
 export default nextConfig;
